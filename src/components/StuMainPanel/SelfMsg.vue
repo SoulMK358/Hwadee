fc002a314c0b141c09ac1c065221486a3951700b
Soulmk <1518976303@qq.com>
2024-06-03 21:45:23 +0800

    stage2:学生注册人脸识别登录

--- src/components/StuMainPanel/SelfMsg.vue
+++ src/components/StuMainPanel/SelfMsg.vue
@@ -1,6 +1,6 @@
 <template>
 
-  <div class="el-card-outside">
+  <el-container class="el-card-outside">
     <br>
     <!--      个人信息栏-->
     <el-card class="el-card-selfMsg">
@@ -15,6 +15,11 @@
         <el-form-item label="所属院校:">
           <el-input placeholder="" v-model="Stu.schoolName" :disabled="true"></el-input>
         </el-form-item>
+        <br>
+        <el-form-item label="人脸识别：">
+          <el-button @click="faceRegisterOpen">点击注册</el-button>
+        </el-form-item>
+
       </el-form>
       <br><br>
       <span style="display: flex; ">已报考科目</span><br>
@@ -25,33 +30,26 @@
       </el-table>
     </el-card>
     <br>
-<!--    &lt;!&ndash;      报考学校信息栏目&ndash;&gt;-->
-<!--    <el-card class="el-card-aimSchool">-->
-<!--      <el-form size="large" label-width="110px">-->
-<!--        <span>个人报考<br><br></span>-->
-<!--        <el-form-item label="考试科目/考场:">-->
-<!--          <el-cascader v-model="selectedCourseSchool" :options="examClass" @change="selectExamClass"/>-->
-<!--          <el-button type="success" @click="confirmCourseSchool">确认科目/考场</el-button>-->
-<!--        </el-form-item>-->
-<!--        <el-form-item label="进度流程:">-->
-<!--          <el-steps style="max-width: 1200px" :active="courseStatus.step" align-center>-->
-<!--            <el-step title="未开始报名" description=""/>-->
-<!--            <el-step title="选择科目/考场" description=""/>-->
-<!--            <el-step title="个人缴费" description=""/>-->
-<!--            <el-step title="确认报名" description=""/>-->
-<!--            <el-step title="打印准考证" description=""/>-->
-<!--            <el-step title="考试结束" description=""/>-->
-<!--          </el-steps>-->
-<!--        </el-form-item>-->
-<!--        <el-form-item>-->
-<!--          <el-button type="success">个人缴费</el-button>-->
-<!--          <el-button type="success">打印座位通知单</el-button>-->
-<!--          <el-button type="success">打印考生准考证</el-button>-->
-<!--          <el-button type="danger">报名</el-button>-->
-<!--        </el-form-item>-->
-<!--      </el-form>-->
-<!--    </el-card>-->
-  </div>
+
+    <!-- 弹出框组件 -->
+    <el-dialog
+      title="人脸识别注册"
+      :visible.sync="dialogVisible"
+      :before-close="handleClose"
+      width="50%">
+      <div>
+        <video ref="video" width="0" height="0" autoplay></video>
+        <canvas ref="canvas" v-show="cameraStatus" width="480" height="360" style="display: flex;margin-bottom: 40px; margin-left: 50px;"></canvas>
+      </div>
+      <div style="display: flex; align-items: center; align-content: center">
+        <el-button type="primary" v-show="cognitionStatus" @click="startCamera">开始注册</el-button><br>
+        <el-button type="danger" v-show="!cognitionStatus" @click="stopCamera">停止识别</el-button>
+        <el-button type="primary" v-show="!cognitionStatus" @click="faceRegister">确认上传</el-button>
+      </div>
+    </el-dialog>
+  </el-container>
+
+
 
 </template>
 
@@ -81,6 +79,21 @@ export default {
         4:"打印准考证(4/5)",
         5:"考试结束(5/5)",
       },
+      // 控制弹出框显示隐藏
+      dialogVisible: false,
+      //控制识别按钮的显示
+      cognitionStatus: true,
+      //摄像机流与绘制暂存对象
+      stream: null,
+      video: null,
+      canvas: null,
+      ctx: null,
+      //控制摄像机显示
+      cameraStatus:false,
+      //循环绘制事件对象
+      canvasId: null,
+      //循环人脸识别登录事件对象
+      faceLoginId: null,
     }
   },
   methods: {
@@ -107,6 +120,111 @@ export default {
           }
         }
       })
+    },
+    //调用摄像头
+    startCamera() {
+      this.cameraStatus=true;
+      this.cognitionStatus = !this.cognitionStatus
+      this.video = this.$refs.video;
+      this.canvas = this.$refs.canvas;
+      this.ctx = this.canvas.getContext('2d');
+      navigator.mediaDevices.getUserMedia({ video: true })
+        .then(stream => {
+          this.stream = stream;
+          this.video.srcObject = stream;
+
+          // 开始绘制翻转的视频帧
+          this.canvasId = setInterval(this.flipAndDraw, 20); // 每20毫秒绘制一次
+          // 开始人脸识别注册尝试
+          // this.faceLoginId = setInterval(this.faceRegister, 2000);//每2s进行一次登录尝试
+        })
+        .catch(error => {
+          console.error("Something went wrong!", error);
+        });
+    },
+    //翻转摄像头图像
+    flipAndDraw() {
+      // 清空画布
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+      // 绘制翻转的视频帧
+      this.ctx.save(); // 保存当前的绘图状态
+      this.ctx.translate(this.canvas.width, 0); // 移动画布
+      this.ctx.scale(-1, 1); // 水平翻转
+      this.ctx.drawImage(this.video, 0, 0, this.canvas.width, this.canvas.height);
+      this.ctx.restore(); // 恢复之前保存的绘图状态
+    },
+    //关闭摄像头
+    stopCamera() {
+
+      if (this.stream) {
+        // 停止视频流的所有轨道
+        this.stream.getTracks().forEach(track => track.stop());
+        // 清除视频源
+        this.video.srcObject = null;
+        // 重置 stream 属性
+        this.stream = null;
+        //关闭20ms执行的摄像头翻转绘制函数
+        clearInterval(this.canvasId);
+        //关闭每2s执行的登录尝试函数
+        // clearInterval(this.faceLoginId);
+
+        this.cameraStatus = false
+
+        this.cognitionStatus = !this.cognitionStatus
+      }
+
+    },
+    //人脸登录注册窗口打开
+    faceRegisterOpen(){
+      this.dialogVisible = true;
+
+    },
+    //人脸识别注册事件
+    faceRegister(){
+      var _this = this
+      this.$confirm('确定上传该人脸信息吗？')
+        .then(() => {
+          // 从 canvas 获取图像数据
+          this.canvas.toBlob((blob)=>{
+            var formData = new FormData();
+            formData.append('file', blob)
+            formData.append('idCard', _this.Stu.idCard)
+
+            this.$axios({
+              method:"post",
+              url:"/face/addFaceData",
+              data: formData
+            }).then(res=>{
+              console.log(res)
+
+              _this.$message({
+                type: res.data.isSuccess ? "success" : "error",
+                duration: 1000,
+                message: res.data.message
+              })
+              if(res.data.isSuccess){
+                _this.stopCamera();
+                this.dialogVisible = false
+              }
+            })
+          })
+        })
+        .catch(() => {});
+
+
+
+    },
+    //关闭摄像头框事件
+    handleClose(){
+      var _this = this
+      this.$confirm('确定要关闭弹出框吗？')
+        .then(() => {
+          this.stopCamera();
+          this.dialogVisible = false;
+        })
+        .catch(() => {});
+
     }
 
   },
@@ -117,6 +235,9 @@ export default {
     //读取学生已选的课程信息
     this.getStuSelectedCourse()
 
+  },
+  beforeDestroy() {
+    this.stopCamera()
   }
 }
 </script>
