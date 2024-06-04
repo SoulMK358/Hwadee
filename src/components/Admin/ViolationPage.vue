698697376906be937d864975c9defee6d5874f66
Soulmk <1518976303@qq.com>
2024-06-04 14:38:44 +0800

    stage2:管理员违规上报页面样式优化

--- src/components/Admin/ViolationPage.vue
+++ src/components/Admin/ViolationPage.vue
@@ -19,57 +19,43 @@
 
       <!-- 主要内容 -->
       <el-main>
-        <div style="margin-bottom: 20px;">
-          <h2>违规上报</h2>
-          <p>上报违规情况，查询违规内容。</p>
-        </div>
 
         <div style="display: flex; justify-content: center; align-items: center; margin-bottom: 20px;">
           <el-input v-model="singleData.idCard" placeholder="身份证号" style="margin-right: 10px;"></el-input>
-<!--          <el-input v-model="singleData.stuName" placeholder="姓名" style="margin-right: 10px;"></el-input>-->
           <el-input v-model="singleData.schoolName" placeholder="学校" style="margin-right: 10px;"></el-input>
           <el-input v-model="singleData.examRoomName" placeholder="考场" style="margin-right: 10px;"></el-input>
-<!--          <el-input v-model="singleData.courseName" placeholder="科目" style="margin-right: 10px;"></el-input>-->
           <el-input v-model="singleData.punishReason" placeholder="违规说明" style="margin-right: 10px;"></el-input>
-
           <el-button type="primary" @click="addRecord" style="margin-right: 10px;">新增</el-button>
           <el-button type="success" @click="exportData">导出</el-button>
         </div>
 
-        <el-card style="margin: 0 auto; width: 90%; padding: 0;">
-          <div style="display: flex; justify-content: center; align-items: center;padding-left: 53px">
-            <el-table :data="tableData.slice((currentPage - 1) * pageSize, currentPage * pageSize)" style="width: 90%;">
+        <el-card style="margin: 0 auto; width: 98%; padding: 0;">
+          <div style="display: flex; justify-content: center; align-items: center;">
+            <el-table :data="tableData.slice((currentPage - 1) * pageSize, currentPage * pageSize)" style="width: 100%;" :row-style="{'text-align': 'center'}">
               <el-table-column v-if="false" prop="punishId" label="punishId" width="100"></el-table-column>
-              <el-table-column prop="idCard" label="身份证号" width="150"></el-table-column>
-              <el-table-column prop="stuName" label="姓名" width="140"></el-table-column>
-              <el-table-column prop="schoolName" label="学校" width="120"></el-table-column>
-              <el-table-column prop="examRoomName" label="考场" width="100"></el-table-column>
-              <el-table-column prop="courseName" label="科目" width="150"></el-table-column>
-              <el-table-column prop="punishReason" label="违规说明" width="150"></el-table-column>
-              <el-table-column label="操作" width="150" align="center">
+              <el-table-column prop="idCard" label="身份证号" width="215" align="center"></el-table-column>
+              <el-table-column prop="stuName" label="姓名" width="160" align="center"></el-table-column>
+              <el-table-column prop="schoolName" label="学校" width="160" align="center"></el-table-column>
+              <el-table-column prop="examRoomName" label="考场" width="160" align="center"></el-table-column>
+              <el-table-column prop="courseName" label="科目" width="160" align="center"></el-table-column>
+              <el-table-column prop="punishReason" label="违规说明" width="200" align="center"></el-table-column>
+              <el-table-column label="操作" width="80" align="center">
                 <template slot-scope="scope">
-<!--                  <el-button size="mini" type="success" @click="editRecord(scope.row)" style="margin-right: 10px;">编辑</el-button>-->
                   <el-button size="mini" type="danger" @click="deleteRecord(scope.row)">删除</el-button>
                 </template>
               </el-table-column>
             </el-table>
           </div>
+
           <el-pagination
             background
-            layout="prev, pager, next,jumper, ->, total"
+            layout="prev, pager, next, jumper, ->, total"
             :total="tableData.length"
             @current-change="handleCurrentChange"
             :current-page="currentPage"
             :page-size="pageSize"
-            style="text-align: center">
+            style="text-align: center;">
           </el-pagination>
-
-<!--          <el-pagination-->
-<!--            background-->
-<!--            layout="prev, pager, next"-->
-<!--            :total="tableData.length"-->
-<!--            style="text-align: center; margin-top: 20px;">-->
-<!--          </el-pagination>-->
         </el-card>
       </el-main>
     </el-container>
@@ -113,19 +99,19 @@ export default {
       //当前页 刷新后默认显示第一页
       currentPage: '1',
       //每一页显示的数据量 此处每页显示6条数据
-      pageSize: '6',
+      pageSize: '8',
     };
   },
   methods: {
     // 新增按钮
     addRecord() {
-      var _this = this
+      var _this = this;
       var datas = {
-        examReason:this.singleData.punishReason,
-        schoolName:this.singleData.schoolName,
+        examReason: this.singleData.punishReason,
+        schoolName: this.singleData.schoolName,
         idCard: this.singleData.idCard,
         examRoomName: this.singleData.examRoomName
-      }
+      };
 
       this.$confirm("是否确认添加该条违规数据","提示",{
         iconClass: "el-icon-question",//自定义图标样式
@@ -135,18 +121,18 @@ export default {
         type: "warning",//提示类型  success/info/warning/error
       }).then(()=>{
         this.$axios({
-          method:"post",
-          url:"/addPunish",
+          method: "post",
+          url: "/addPunish",
           data: JSON.stringify(datas)
         }).then(res=>{
-          console.log(res)
+          console.log(res);
           // 弹窗显示后端返回的信息（成功、失败原因）
           _this.$message({
             type: res.data.code == 200 ? "success" : "error",
             message: res.data.message
-          })
+          });
 
-          if(res.data.code == 200){
+          if (res.data.code == 200) {
             setTimeout(function (){
               _this.singleData = {
                 punishId:1,
@@ -156,19 +142,16 @@ export default {
                 examRoomName:'',
                 punishReason:'',
                 courseName:''
-              }
-              _this.getPunishList()
-            }, 500)
+              };
+              _this.getPunishList();
+            }, 500);
           }
-        })
+        });
       }).catch(()=>{
 
-      })
-
-
-
+      });
     },
-    // 导出数据为excle表格
+    // 导出数据为excel表格
     exportData() {
       // 导出按钮
       // 获取表单数据
@@ -178,13 +161,13 @@ export default {
       const data = [];
       for (let value of formData) {
         var t = {
-          punishId : value.punishId,
-          "身份证号":value.idCard,
-          "姓名":value.stuName,
-          "考场":value.examRoomName,
-          "科目":value.courseName,
-          "违规说明":value.punishReason,
-        }
+          "身份证号": value.idCard,
+          "姓名": value.stuName,
+          "学校": value.schoolName,
+          "考场": value.examRoomName,
+          "科目": value.courseName,
+          "违规说明": value.punishReason,
+        };
         data.push(t);
       }
 
@@ -200,7 +183,7 @@ export default {
     },
     // 删除按钮
     deleteRecord(row) {
-      var _this = this
+      var _this = this;
 
       this.$confirm("是否确认删除该条违规数据","提示",{
         iconClass: "el-icon-question",//自定义图标样式
@@ -211,36 +194,35 @@ export default {
       }).then(()=>{
         //确认操作
         this.$axios({
-          method:"delete",
-          url:"deletePunish?punishId=" + row.punishId,
+          method: "delete",
+          url: "deletePunish?punishId=" + row.punishId,
         }).then(res=>{
-          console.log(res)
+          console.log(res);
           // 弹窗显示后端返回的信息（成功、失败原因）
           _this.$message({
             type: res.data.code == 200 ? "success" : "error",
             message: res.data.message
-          })
-          if(res.data.code == 200){
+          });
+          if (res.data.code == 200) {
             setTimeout(function (){
-              _this.getPunishList()
-            },500)
+              _this.getPunishList();
+            }, 500);
           }
-        })
+        });
       }).catch(() => {
         //取消操作
-      })
+      });
     },
     //获得惩罚名单
     getPunishList(){
-      var _this = this
-
+      var _this = this;
       this.$axios({
-        method:"post",
-        url:"/punishList"
+        method: "post",
+        url: "/punishList"
       }).then(res=>{
-        console.log(res.data)
-        _this.tableData = res.data
-      })
+        console.log(res.data);
+        _this.tableData = res.data;
+      });
     },
     //点击按钮切换页面
     handleCurrentChange(currentPage) {
@@ -249,11 +231,9 @@ export default {
     }
   },
   mounted() {
-    var parse = JSON.parse(localStorage.getItem("currentAdmin"))
-    this.userName = parse.adminName
-
-    this.getPunishList()
-
+    var parse = JSON.parse(localStorage.getItem("currentAdmin"));
+    this.userName = parse.adminName;
+    this.getPunishList();
   }
 };
 </script>
@@ -275,4 +255,3 @@ export default {
   color: #333;
 }
 </style>
-
